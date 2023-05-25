917c10b1deea636306cffe2e5a68e26cf4f63bc4
takanashi@gclue.jp <takanashi@gclue.jp>
2023-05-25 10:49:24 +0900

    add sudo in run.sh

--- run-pc-ubuntu1804-carla.sh
+++ run-pc-ubuntu1804-carla.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+########################################
+# Rerun the script with root
+########################################
+if [ "$EUID" -ne 0 ]; then
+  sudo "$0" "$@"  # execute script with root
+  exit            # exit this user script
+fi
+
 HOST_USER=$(getent passwd 1000 | cut -d: -f1)
 HOST_USER_GROUP=$(getent group 1000 | cut -d: -f1)
 HOST_USER_HOME=/home/$HOST_USER
@@ -14,7 +23,8 @@ DISPLAY=`echo $DISPLAY`
 if [ -z $DISPLAY ]; then
     # localhost display
     # Ubuntu 18.04: :0
-    # Ubuntu 20.04: :1
+    # Ubuntu 20.04: :1 or 0
+    # Ubuntu 22.04: :0
     DISPLAY=:0
 fi
 
