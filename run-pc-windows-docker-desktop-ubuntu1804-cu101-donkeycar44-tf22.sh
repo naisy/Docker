a664166927fce5450d00cc712292ce9e2979fac9
takanashi@gclue.jp <takanashi@gclue.jp>
2024-12-04 17:07:28 +0900

    add files

---
+++ run-pc-windows-docker-desktop-ubuntu1804-cu101-donkeycar44-tf22.sh
@@ -0,0 +1,34 @@
+# 必要な変数を設定
+#Power Shell 5->7
+# PS5: --mount type=bind,source=$HOST_MOUNT_PATH,target=$DOCKER_MOUNT_PATH `
+# PS7: --mount "type=bind,source=$HOST_MOUNT_PATH,target=$DOCKER_MOUNT_PATH" `
+
+$IMG = 'naisy/pc-ubuntu1804-donkeycar44-tf22'
+$PORT = 8888  # JupyterLabが動作するポート番号
+$NAME = 'donkeycar44-tf22'  # Dockerコンテナ名
+$HOST_MOUNT_PATH = 'C:/App/data'  # C:/App/dataフォルダをDockerコンテナの/home/ubuntu/data ($HOME/data)としてマウントする
+$DOCKER_MOUNT_PATH = '/home/ubuntu/data'
+$DISPLAY = 'host.docker.internal:0.0'  # DISPLAY値
+
+# ホスト側のディレクトリが存在しない場合、作成する
+if (-not (Test-Path -Path $HOST_MOUNT_PATH)) {
+    New-Item -ItemType Directory -Path $HOST_MOUNT_PATH
+}
+
+# Docker コンテナを起動
+docker run `
+    --gpus all `
+    --restart always `
+    -it `
+    --mount "type=bind,source=$HOST_MOUNT_PATH,target=$DOCKER_MOUNT_PATH" `
+    --mount "type=bind,source=/dev/,target=/dev/" `
+    -e "DISPLAY=$DISPLAY" `
+    -e "QT_GRAPHICSSYSTEM=native" `
+    -e "QT_X11_NO_MITSHM=1" `
+    -e "TF_FORCE_GPU_ALLOW_GROWTH=true" `
+    -e "SHELL=/bin/bash" `
+    -e "TZ=Asia/Tokyo" `
+    --network=host `
+    --name "$NAME" `
+    "$IMG" `
+bash -c "source /virtualenv/python3/bin/activate && jupyter-lab --ip=0.0.0.0 --no-browser --ServerApp.root_dir=/ --LabApp.default_url=/lab?file-browser-path=/home/ubuntu/"
--- run-pc-windows-docker-desktop-ubuntu2204-trt.sh
+++ run-pc-windows-docker-desktop-ubuntu2204-trt.sh
@@ -1,15 +1,10 @@
 # 必要な変数を設定
-#Power Shell 5->7
-# PS5: --mount type=bind,source=$HOST_MOUNT_PATH,target=$DOCKER_MOUNT_PATH `
-# PS7: --mount "type=bind,source=$HOST_MOUNT_PATH,target=$DOCKER_MOUNT_PATH" `
 
 $IMG = 'naisy/pc-ubuntu2204-trt-base'
-$PORT = 9888  # JupyterLabが動作するポート番号 (http://localhost:9888)
+$PORT = 8888  # JupyterLabが動作するポート番号 (http://localhost:8888)
 $NAME = 'trt'  # Dockerコンテナ名 重複起動しないようにユニークな名前を付けておく
 $HOST_MOUNT_PATH = 'C:/App/data'  # C:/App/dataフォルダをDockerコンテナの/home/ubuntu/data ($HOME/data)としてマウントする
 $DOCKER_MOUNT_PATH = "/home/ubuntu/data"
-$DISPLAY = "host.docker.internal:0.0"  # または適切な DISPLAY 値に変更
-$USB_DEVICE_PATH = "/dev/bus/usb/001/002"  # カメラのパス (確認した lsusb の結果に基づく)
 
 # ホスト側のディレクトリが存在しない場合、作成する
 if (-not (Test-Path -Path $HOST_MOUNT_PATH)) {
@@ -22,7 +17,6 @@ docker run `
     -itd `
     --mount "type=bind,source=$HOST_MOUNT_PATH,target=$DOCKER_MOUNT_PATH" `
     --mount type=bind,source=/dev/,target=/dev/ `
-    -e DISPLAY=$DISPLAY `
     -e QT_GRAPHICSSYSTEM=native `
     -e QT_X11_NO_MITSHM=1 `
     -e TF_FORCE_GPU_ALLOW_GROWTH=true `
