843014fb46b1294a6c9fc69b70358d49d44b11e0
takanashi@gclue.jp <takanashi@gclue.jp>
2025-08-26 13:55:29 +0900

    update

--- run-pc-windows-docker-desktop-ubuntu2404-trt.sh
+++ run-pc-windows-docker-desktop-ubuntu2404-trt.sh
@@ -22,6 +22,19 @@ wsl mkdir -p $UBUNTU_HOST_MOUNT_PATH
 # WSL2 のパスを Windows 形式に変換
 $WSL2_HOST_MOUNT_PATH = wsl wslpath -w $UBUNTU_HOST_MOUNT_PATH
 
+# bashに渡すコマンドはHere-Stringで作る（PowerShellの`解釈を避ける）
+$BashCmd = @"
+source /virtualenv/python3/bin/activate
+jupyter lab \
+  --ip=0.0.0.0 \
+  --port=$PORT \
+  --no-browser \
+  --ServerApp.iopub_msg_rate_limit=10000 \
+  --ServerApp.iopub_data_rate_limit=10000000 \
+  --ServerApp.root_dir=/ \
+  --LabApp.default_url=/lab?file-browser-path=/home/ubuntu
+"@
+
 docker run `
     --gpus all `
     --restart always `
@@ -29,6 +42,7 @@ docker run `
     --mount "type=bind,source=$WINDOWS_HOST_MOUNT_PATH,target=$DOCKER_WINDOWS_HOST_MOUNT_PATH" `
     --mount "type=bind,source=$WSL2_HOST_MOUNT_PATH,target=$DOCKER_UBUNTU_HOST_MOUNT_PATH" `
     --mount type=bind,source=/dev/,target=/dev/ `
+    --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock `
     -e DISPLAY=$DISPLAY `
     -e QT_GRAPHICSSYSTEM=native `
     -e QT_X11_NO_MITSHM=1 `
@@ -37,7 +51,8 @@ docker run `
     -e SHELL=/bin/bash `
     -e TZ=Asia/Tokyo `
     -p ${PORT}:${PORT} `
+    --add-host host.docker.internal:host-gateway `
     --name $NAME `
     $IMG `
-bash -c "source /virtualenv/python3/bin/activate && jupyter lab --ip=0.0.0.0 --port=$PORT --no-browser --ServerApp.iopub_msg_rate_limit=10000 --ServerApp.iopub_data_rate_limit=10000000 --ServerApp.root_dir=/ --LabApp.default_url=/lab?file-browser-path=/home/ubuntu"
+bash -lc "$BashCmd"
 
